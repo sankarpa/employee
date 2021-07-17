b99d1780c394d225369a4a7b442165701166b44d
Sankar Parameswaran <sankarpa@thoughtworks.com>
2021-07-17 19:19:48 +0530

    fail the unit test

--- src/main/java/com/example/employee/controller/PingController.java
+++ src/main/java/com/example/employee/controller/PingController.java
@@ -14,7 +14,7 @@ public class PingController {
 
     @GetMapping("/ping")
     public ResponseEntity ping(){
-        return ResponseEntity.ok().body("pong "+profile);
+        return ResponseEntity.ok().body("pong");
     }
 
 }
